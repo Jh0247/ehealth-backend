b44d2251e02d84fef144623259dd801fcfe6d9ee
Jh <cjuohan47@gmail.com>
2024-07-07 15:27:16 +0800

    change cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,7 +16,7 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Origin', 'http://52.64.17.175:5173')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
             ->header('Access-Control-Allow-Credentials', 'true');
